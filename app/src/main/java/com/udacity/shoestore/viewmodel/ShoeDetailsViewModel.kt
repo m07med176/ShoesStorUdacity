d46119c2fcc5248b00d309d922abfd7c24c63f62
Mohamed <dev.mohamed.arafa@gmail.com>
2022-11-25 19:56:42 +0200

    Shoe Detail UI done

--- app/src/main/java/com/udacity/shoestore/viewmodel/ShoeDetailsViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/viewmodel/ShoeDetailsViewModel.kt
@@ -12,4 +12,28 @@ class ShoeDetailsViewModel : ViewModel() {
     val shoeCompany = MutableLiveData<String>()
     val shoeDescription = MutableLiveData<String>()
 
+    fun validateFields() : Boolean {
+        return fieldNotEmpty(shoeName) &&
+                fieldNotEmpty(shoeSize) &&
+                fieldNotEmpty(shoeCompany) &&
+                fieldNotEmpty(shoeDescription)
+    }
+
+    private fun fieldNotEmpty(data: MutableLiveData<String>) : Boolean {
+        return data.value != null && data.value?.isNotEmpty() == true
+    }
+
+    private fun shoeSizeAsDouble() : Double {
+        if (!fieldNotEmpty(shoeSize)) return 0.0
+        return shoeSize.value?.toDouble()!!
+    }
+
+    fun buildShoe() : Shoe {
+        return Shoe(
+            shoeName.value ?: "",
+            shoeSizeAsDouble(),
+            shoeCompany.value ?: "",
+            shoeDescription.value ?: "",
+            arrayListOf(""))
+    }
 }
