ed3ea77cbc2a2083ba55deda3ef1106e9bcd45c1
Mohamed <dev.mohamed.arafa@gmail.com>
2022-11-25 17:02:21 +0200

    fix bug

--- app/src/main/java/com/udacity/shoestore/screens/LoginScreen.kt
+++ app/src/main/java/com/udacity/shoestore/screens/LoginScreen.kt
@@ -1,14 +1,16 @@
 package com.udacity.shoestore.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginScreenBinding
+
 
 class LoginScreen : Fragment() {
     override fun onCreateView(
@@ -22,7 +24,7 @@ class LoginScreen : Fragment() {
             container,
             false)
 
-        activity!!.actionBar!!.hide()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
 
         binding.buttonSignUp.setOnClickListener {
             findNavController().navigate(LoginScreenDirections.actionLoginFragmentToWelcomeFragment())
