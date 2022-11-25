49f22302414ff42bb6c627b576799c2d83d84171
Mohamed <dev.mohamed.arafa@gmail.com>
2022-11-25 10:29:24 +0200

    Login Screen Ui

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -24,8 +24,6 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-
-
         Timber.plant(Timber.DebugTree())
     }
 
