d27d8aad9753e8fc14513927c07409f6f46cd09e
Mohamed <dev.mohamed.arafa@gmail.com>
2022-11-25 20:15:33 +0200

    Shoe Details View Model Settings

--- app/src/main/java/com/udacity/shoestore/screens/ShoeDetailScreen.kt
+++ app/src/main/java/com/udacity/shoestore/screens/ShoeDetailScreen.kt
@@ -5,22 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginScreenBinding
 import com.udacity.shoestore.databinding.FragmentShoeDetailScreenBinding
+import com.udacity.shoestore.viewmodel.ShoeDetailsViewModel
+import com.udacity.shoestore.viewmodel.ShoesListViewModel
 
 class ShoeDetailScreen : Fragment() {
+    val shoesViewModel: ShoesListViewModel by activityViewModels()
+    lateinit var shoeDetailsViewModel: ShoeDetailsViewModel
+    lateinit var binding: FragmentShoeDetailScreenBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentShoeDetailScreenBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_shoe_detail_screen,
             container,
             false)
+
+        shoeDetailsViewModel = ViewModelProvider(this)[ShoeDetailsViewModel::class.java]
+        binding.viewModel = shoeDetailsViewModel
+        binding.lifecycleOwner = this
 
         return binding.root
     }
