85212c7ef324a29d689af2cf1a7d311a29cdd6ac
Mohamed <dev.mohamed.arafa@gmail.com>
2022-11-25 19:26:39 +0200

    Item View List Created and Ajusted in ShoseListScreen

--- app/src/main/java/com/udacity/shoestore/screens/ShoeListingScreen.kt
+++ app/src/main/java/com/udacity/shoestore/screens/ShoeListingScreen.kt
@@ -6,23 +6,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.udacity.shoestore.R
-import com.udacity.shoestore.databinding.FragmentShoeDetailScreenBinding
+import com.udacity.shoestore.viewmodel.ShoesListViewModel
 import com.udacity.shoestore.databinding.FragmentShoeListingScreenBinding
-
+import com.udacity.shoestore.databinding.ItemListShoeBinding
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.models.Shoe
 
 class ShoeListingScreen : Fragment() {
+
+    val viewModel: ShoesListViewModel by activityViewModels()
+    lateinit var binding: FragmentShoeListingScreenBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentShoeListingScreenBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_shoe_listing_screen,
             container,
             false)
 
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(ShoeListingScreenDirections.actionShoeListFragmentToShoeDetailFragment())
+        }
         return binding.root
+    }
+
+    private fun addShoeToView(
+        container: ViewGroup?,
+        shoe: Shoe
+    ) {
+        val itemViewList: ItemListShoeBinding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.item_list_shoe, container, false
+        )
+        itemViewList.shoe = shoe
+        binding.itemList.addView(itemViewList.root)
     }
 }
